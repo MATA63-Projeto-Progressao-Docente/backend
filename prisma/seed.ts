0aa063b99735b9325a306d24f55f100dac732f2a
Matheus Novais <matheusnovaisz@gmail.com>
2023-12-06 00:29:40 -0300

    add: seeds for entities

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
 import prisma from './client';
 import activitiesJson from './jsons/activities.json';
 import fieldsJson from './jsons/fields.json';
+import departmentsJson from './jsons/departments.json';
+import usersJson from './jsons/users.json';
+import classesJson from './jsons/classes.json';
+import professorsJson from './jsons/professor.json';
 
 async function seedFields() {
   const promises = fieldsJson.map(async (field) => prisma.field.upsert({
@@ -28,9 +32,71 @@ async function seedActivities() {
   await Promise.all(promises);
 }
 
+async function seedClasses() {
+  const promises = classesJson.sort((a, b) => a.id - b.id)
+    .map(async (classItem) => prisma.class.create({
+      data: {
+        id: classItem.id,
+        name: classItem.name,
+        letter: classItem.letter,
+        level: classItem.level,
+      },
+    }));
+  await Promise.all(promises);
+}
+
+async function seedUsers() {
+  const promises = usersJson.map(async (user) => prisma.user.upsert({
+    where: { email: user.email },
+    update: {},
+    create: {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      role: 'USER',
+    },
+  }));
+  await Promise.all(promises);
+}
+
+async function seedDepartments() {
+  const promises = departmentsJson.map(async (department) => prisma.department.create({
+    data: {
+      name: department.name,
+      headUserId: department.headUserId,
+    },
+  }));
+  await Promise.all(promises);
+}
+
+async function seedProfessors() {
+  const promises = professorsJson.map(async (professor) => {
+    const user = await prisma.user.findFirst({
+      where: {
+        name: professor.name,
+      },
+    });
+    if (!user) { return null; }
+    return prisma.professor.create({
+      data: {
+        name: professor.name,
+        classId: professor.classId,
+        departmentId: professor.departmentId,
+        category: 'EXCLUSIVE',
+        userId: user.id,
+      },
+    });
+  });
+  await Promise.all(promises);
+}
+
 async function main() {
   await seedFields();
   await seedActivities();
+  await seedUsers();
+  await seedClasses();
+  await seedDepartments();
+  await seedProfessors();
 
   await prisma.user.upsert({
     where: { email: 'admin@ufba.br' },
