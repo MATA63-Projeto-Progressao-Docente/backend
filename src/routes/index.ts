d02273262e0618cada3794e1f087f8fbdd0e6f6c
Matheus Novais <matheusnovaisz@gmail.com>
2023-12-06 00:33:27 -0300

    add: route for classes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import * as authController from '../controllers/authController';
 import * as fieldController from '../controllers/fieldController';
+import * as classesController from '../controllers/classesController';
 import validateToken from '../middlewares/validateToken';
 import processRouter from './processRoutes';
 import activityRouter from './activityRoutes';
@@ -22,5 +23,7 @@ router.use('/users', validateToken, usersRoutes);
 router.get('/fields', validateToken, fieldController.getAllFields);
 
 router.get('/fields/:fieldId', validateToken, fieldController.getField);
+
+router.get('/classes', classesController.getAll);
 
 export default router;
